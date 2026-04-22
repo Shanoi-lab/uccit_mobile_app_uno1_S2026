7e38f3d64629d63e0120cde5fadca37cbaec8cdb
Shanoi-lab <shanoiwilson.sw@gmail.com>
2026-04-21 20:55:14 -0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     @laravelPWA
 
     {{-- Google Fonts --}}
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="manifest" href="/manifest.json">
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600&family=Space+Grotesk:wght@500;700&display=swap" rel="stylesheet">
 
     {{-- App Stylesheet --}}
